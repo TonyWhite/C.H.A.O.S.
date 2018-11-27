7c5ffe2bd55c961fa50f32395e2dd262abc6c34e
Antonio Bianco <antonio@localhost.localdomain>
2018-11-27 23:55:27 +0100

    Translate key notes and renamed variables.

--- sandbox/data block/blocks.cpp
+++ sandbox/data block/blocks.cpp
@@ -1,9 +1,3 @@
-// The data must be a multiple of 16 bytes.
-// If it is not, I have to adapt the last block by adding zeros.
-// Now it is a multiple of 16 bytes.
-// I add another 16-byte block (control block) to indicate if I have corrected the last block and what was its real size.
-
-
 #include <iostream>
 #include <string>
 
@@ -11,37 +5,44 @@ using std::cout;
 using std::endl;
 using std::string;
 
-int main()
+/*
+ * Arrange data to be a multiple of 64 bit
+ * The data must be a multiple of 16 bytes.
+ * If it is not, I have to adapt the last block by adding zeros.
+ * Now it is a multiple of 16 bytes.
+ * I add another 16-byte block (control block) to indicate if I have corrected the last block and what was its real size.
+ */
+int arrange_data(string &data, bool debug = false)
 {
-  unsigned int block_size = 16; // Size in Bytes: 128 bit
+  int error = 0;
+  unsigned int aes_block_size = 16; // Size in Bytes: 128 bit
 
-  string data = "0123456789ABCDEF0123456789ABCDEF0";
   unsigned int data_length = data.length(); // Size in Bytes
-  /* DEBUG */ cout << "Data size: " << data_length << " Bytes" << endl;
+  if(debug) cout << "Data size: " << data_length << " Bytes" << endl;
 
   if(data_length)
   {
-    unsigned int full_blocks = data_length/block_size;
-    /* DEBUG */ cout << "Full blocks: " << full_blocks << endl;
-    unsigned int last_data_block_length = data_length - full_blocks * block_size;
+    unsigned int full_blocks = data_length/aes_block_size;
+    if(debug) cout << "Full blocks: " << full_blocks << endl;
+    unsigned int last_data_block_length = data_length - full_blocks * aes_block_size;
 
     if(last_data_block_length) // Last block incomplete
     {
       // Complete the block with zero bytes
-      /* DEBUG */ cout << "Last data block length: " << last_data_block_length << " Bytes" << endl;
+      if(debug) cout << "Last data block length: " << last_data_block_length << " Bytes" << endl;
       unsigned char zero_byte = '\0'; // Binary value = 00000000
-      unsigned int bytes_left = block_size - last_data_block_length;
+      unsigned int bytes_left = aes_block_size - last_data_block_length;
       while(bytes_left)
       {
         data += zero_byte;
         bytes_left--;
       }
       data_length = data.length(); // Read new size in Bytes
-      /* DEBUG */ cout << "New data size: " << data_length << " Bytes" << endl;
+      if(debug) cout << "New data size: " << data_length << " Bytes" << endl;
     }
     else // Last block complete
     {
-      /* DEBUG */ cout << "The Data contains only full blocks" << endl;
+      if(debug) cout << "The Data contains only full blocks" << endl;
     }
 
     // DEBUG: Need to write another complete block with real size of incomplete block (last_data_block_length)
@@ -49,17 +50,29 @@ int main()
   }
   else
   {
-    /* DEBUG */ cout << "Data not found" << endl;
+    error = 1;
+    if(debug) cout << "Data not found" << endl;
   }
+  return error;
+}
 
-  unsigned int valid_key_size[3] = {128, 192, 256}; // valid key size for AES-256
+int arrange_key(string &key, bool debug = false)
+{
+  unsigned int aes_key_size[3] = {128, 192, 256}; // valid key size for AES-256
   // Proposta: utilizzare solo chiavi da 256 bit
   // Medoto:
-  // 1- per chiavi <128 bit, si aggiungono zeri per riempire 128 bit. CONFIDENTIAL
-  // 2- per chiavi =128 bit, si usa la chiave a 128 bit. SECRET
-  // 3- per chiavi >128 & <192 bit, si aggiungono zeri per riempire 192 bit. SECRET
-  // 4- per chiavi =192 bit, si usa la chiave a 192 bit. TOP SECRET LIV. 1
-  // 5- per chiavi >192 & <256 bit, si aggiungono zeri per riempire 256 bit. TOP SECRET
-  // 6- per chiavi =256 bit, si usa la chiave a 256 bit. TOP SECRET LIV. 2
-  // 7- per chiavi >256 bit, si usano i primi 256 bit della chiave. Il resto sarà usato per cifrare il risultato.
+  // 1- key <128 bit, add zeros to fill 128 bit. CONFIDENTIAL
+  // 2- key =128 bit, use 128 bit key. SECRET
+  // 3- key >128 & <192 bit, add zeros to fill 192 bit. SECRET
+  // 4- key =192 bit, use 192 bit key. TOP SECRET LIV. 1
+  // 5- key >192 & <256 bit, add zeros to fill 256 bit. TOP SECRET
+  // 6- key =256 bit, use 256 bit key. TOP SECRET LIV. 2
+  // 7- key >256 bit, the first 256 bits of the key are used. The other bits of the key will be used to encrypt again.
+}
+
+int main()
+{
+  string qualcosa = "0123456789ABCDEF0123456789ABCDEF0";
+  arrange_data(qualcosa, true);
+  cout << "Qualcosa: " << qualcosa.length() << endl;
 }
